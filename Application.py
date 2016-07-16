d9938da025447273f231d992fe2a0ab4fe4167f2
Lukas Praninskas <l.praninskas@adform.com>
2016-07-16 20:04:32 +0300

    Fix errors

--- Application.py
+++ Application.py
@@ -1,25 +1,14 @@
-#!/usr/bin/env python2.7
-
-import redis
 from flask import Flask
 from flask_restful import Resource, Api
-from Session import Session
-from OpenTransaction import OpenTransaction
+
+from Resources.OpenTransaction import OpenTransaction
+from Resources.Session import Session
 
 app = Flask(__name__)
 api = Api(app)
 
-
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-
-redisConnection = redis.StrictRedis(host="localhost", port=6379, db=0)
-
-api.add_resource(HelloWorld, '/')
-api.add_resource(OpenTransaction(redisConnection), "/open")
-api.add_resource(Session(redisConnection), "/session")
+api.add_resource(OpenTransaction, '/')
+api.add_resource(Session, '/')
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True)
