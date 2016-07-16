5cf24949263a2056cb322e234f49c97691368084
Lukas Praninskas <l.praninskas@adform.com>
2016-07-16 20:07:18 +0300

    remove constructor properties

--- Resources/OpenTransaction.py
+++ Resources/OpenTransaction.py
@@ -4,8 +4,5 @@ from flask_restful import Resource
 class OpenTransaction(Resource):
     redis = None
 
-    def __init__(self, redis):
-        self.redis = redis
-
     def post(self):
         return
--- Resources/Session.py
+++ Resources/Session.py
@@ -4,8 +4,5 @@ from flask_restful import Resource
 class Session(Resource):
     redis = None
 
-    def __init__(self, redis):
-        self.redis = redis
-
     def post(self):
         return {"token": "12345456"}
